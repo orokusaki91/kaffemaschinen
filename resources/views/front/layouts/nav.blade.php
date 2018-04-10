48829d6317c4bbe8a6acf44c55e091fdf974ecaa
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-04-10 15:38:25 +0200

    add backend for new navigation

--- resources/views/front/layouts/nav.blade.php
+++ resources/views/front/layouts/nav.blade.php
@@ -8,26 +8,25 @@
         </a>
     </li>
     @foreach($navs as $nav)
-    <li>
-        <!-- <a href="{{ URL::to('shop?slug=' . $nav->slug) }}" class="{{ $__env->yieldContent('nav_active_category') == $nav->slug ? 'active' : '' }}">
-            {{ $nav->name }};
-        </a> -->
-        
-        <li class="dropdown">
-            <a href="#" class="{{ $__env->yieldContent('nav_active_category') == $nav->slug ? 'active' : '' }}">
-            {{ $nav->name }} &#9662;
-        	</a>
-            <ul class="dropdown-menu">
-                <li><a href="#">lorem ipsum</a></li>
-                <li><a href="#">lorem ipsum</a></li>
-                <li><a href="#">lorem ipsum</a></li>
-                <li><a href="#">lorem ipsum</a></li>
-                <li><a href="#">lorem ipsum</a></li>
-            </ul>
+        <?php $childCategories = $nav['children']; ?>
+        <li>
+            <!-- <a href="{{ URL::to('shop?slug=' . $nav->slug) }}" class="{{ $__env->yieldContent('nav_active_category') == $nav->slug ? 'active' : '' }}">
+                {{ $nav->name }};
+            </a> -->
+            <li class="{{ count($childCategories) > 0 ? 'dropdown' : '' }}">
+                
+                <a href="#" class="{{ $__env->yieldContent('nav_active_category') == $nav->slug ? 'active' : '' }}">{{ $nav->name }} {{ count($childCategories) > 0 ? '&#9662;' : '' }}</a>
+                @if(count($childCategories) > 0)
+                    <ul class="dropdown-menu">
+                        @foreach($childCategories as $childCategory)
+                            <li>
+                                <a href="{{ url('shop?slug=' . $childCategory->slug) }}">{{ $childCategory->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+            </li>
         </li>
-        
-      
-    </li>
     @endforeach
     <li>
         <a href="{{ route('page.partner') }}" class="">
